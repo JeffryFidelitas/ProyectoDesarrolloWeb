a2e7e576261fd71de036ce7506d539e998709782
Tohdz <hhernandez70088@ufide.ac.cr>
2024-03-18 23:31:31 -0600

    prueba merge
    hhernan

--- G4_SC403_MN_Anteproyecto - Avance 2/ProyectoFinal/src/main/java/com/ProyectoFinal/ProyectoFinal/dao/gameDao.java
+++
@@ -1,9 +0,0 @@
-package com.ProyectoFinal.ProyectoFinal.dao;
-
-import com.ProyectoFinal.ProyectoFinal.domain.game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface gameDao extends JpaRepository <game,Long>{
-    
-}
-
