feca0335412dab8413a730f16c2c97e8e44c4191
Tohdz <hhernandez70088@ufide.ac.cr>
2024-03-21 14:45:39 -0600

    base_datos_unida
    hhernan

--- G4_SC403_MN_Anteproyecto - Avance 2/fidegames.sql
+++ G4_SC403_MN_Anteproyecto - Avance 2/fidegames.sql
@@ -1,13 +1,15 @@
--- MariaDB dump 10.19-11.3.2-MariaDB, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `fidegames` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `fidegames`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: FideGames
+-- Host: localhost    Database: fidegames
 -- ------------------------------------------------------
--- Server version	11.3.2-MariaDB
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,17 +18,42 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `contacto`
+--
+
+DROP TABLE IF EXISTS `contacto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contacto` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `correo` varchar(40) NOT NULL,
+  `mensaje` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacto`
+--
+
+LOCK TABLES `contacto` WRITE;
+/*!40000 ALTER TABLE `contacto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `emulators`
 --
 
 DROP TABLE IF EXISTS `emulators`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `emulators` (
-  `name` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `year` year(4) NOT NULL,
-  `description` text NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `year` year NOT NULL,
+  `description` text COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`name`),
   UNIQUE KEY `id` (`name`),
   UNIQUE KEY `id_2` (`name`),
@@ -40,8 +67,7 @@ CREATE TABLE `emulators` (
 
 LOCK TABLES `emulators` WRITE;
 /*!40000 ALTER TABLE `emulators` DISABLE KEYS */;
-INSERT INTO `emulators` VALUES
-('SNES','2024-03-05 21:32:03',1990,'The Super NES is Nintendo\'s second programmable home console, following the Nintendo Entertainment System (NES). The console introduced advanced graphics and sound capabilities compared with other systems at the time. It was designed to accommodate the ongoing development of a variety of enhancement chips integrated into game cartridges to be more competitive into the next generation.');
+INSERT INTO `emulators` VALUES ('SNES','2024-03-05 21:32:03',1990,'The Super NES is Nintendo\'s second programmable home console, following the Nintendo Entertainment System (NES). The console introduced advanced graphics and sound capabilities compared with other systems at the time. It was designed to accommodate the ongoing development of a variety of enhancement chips integrated into game cartridges to be more competitive into the next generation.');
 /*!40000 ALTER TABLE `emulators` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,16 +77,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `games`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `games` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `year` year(4) NOT NULL,
-  `emulator` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `extension` varchar(255) NOT NULL DEFAULT 'NULL',
-  `bios` varchar(255) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `year` year NOT NULL,
+  `emulator` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `description` text COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `extension` varchar(255) COLLATE utf8mb4_bin NOT NULL DEFAULT 'NULL',
+  `bios` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -71,9 +97,7 @@ CREATE TABLE `games` (
 
 LOCK TABLES `games` WRITE;
 /*!40000 ALTER TABLE `games` DISABLE KEYS */;
-INSERT INTO `games` VALUES
-(1,'2024-03-05 20:45:14',1990,'snes','Super Mario World is often considered one of the best games in the series and is cited as one of the greatest video games ever made. It sold more than twenty million copies worldwide, making it the best-selling SNES game. It also led to an animated television series of the same name and a 1995 sequel, Yoshi\'s Island. The game has been re-released on multiple occasions: It was part of the 1994 compilation Super Mario All-Stars + Super Mario World for the SNES and was re-released for the Game Boy Advance as Super Mario World: Super Mario Advance 2 in 2001, on the Virtual Console for the Wii, Wii U, and New Nintendo 3DS consoles, and as part of the Super NES Classic Edition. In 2019, it was released for Nintendo Switch Online as part of the classic games service.','Super Mario World','sfc',NULL),
-(2,'2024-03-05 23:18:00',1999,'psx','Pepsiman is an action video game developed and published by KID for the PlayStation. It was released in Japan on March 4, 1999, and is based on the eponymous Japanese superhero mascot for the American carbonated soft drink Pepsi.','Pepsiman','iso','scph5500.bin');
+INSERT INTO `games` VALUES (1,'2024-03-05 20:45:14',1990,'snes','Super Mario World is often considered one of the best games in the series and is cited as one of the greatest video games ever made. It sold more than twenty million copies worldwide, making it the best-selling SNES game. It also led to an animated television series of the same name and a 1995 sequel, Yoshi\'s Island. The game has been re-released on multiple occasions: It was part of the 1994 compilation Super Mario All-Stars + Super Mario World for the SNES and was re-released for the Game Boy Advance as Super Mario World: Super Mario Advance 2 in 2001, on the Virtual Console for the Wii, Wii U, and New Nintendo 3DS consoles, and as part of the Super NES Classic Edition. In 2019, it was released for Nintendo Switch Online as part of the classic games service.','Super Mario World','sfc',NULL),(2,'2024-03-05 23:18:00',1999,'psx','Pepsiman is an action video game developed and published by KID for the PlayStation. It was released in Japan on March 4, 1999, and is based on the eponymous Japanese superhero mascot for the American carbonated soft drink Pepsi.','Pepsiman','iso','scph5500.bin');
 /*!40000 ALTER TABLE `games` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,12 +107,12 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `likes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `likes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` int(11) NOT NULL,
-  `game_id` int(11) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int NOT NULL,
+  `game_id` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -103,19 +127,45 @@ LOCK TABLES `likes` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `noticia`
+--
+
+DROP TABLE IF EXISTS `noticia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `noticia` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `imagen` varchar(1024) NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `cuerpo` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `noticia`
+--
+
+LOCK TABLES `noticia` WRITE;
+/*!40000 ALTER TABLE `noticia` DISABLE KEYS */;
+INSERT INTO `noticia` VALUES (1,'https://media.vandal.net/i/1280x720/3-2024/20/202432011551779_1.jpg.webp','Metal Gear Solid 5: Ground Zeroes, el prologo de The Phantom Pain, cumple 10 años','Metal Gear Solid V: Ground Zeroes ha cumplido diez años. Se trata del prólogo\nde Metal Gear Solid V: The Phantom Pain y llegó tanto a PlayStation 4, Xbox\nOne y PC como a la generación anterior PlayStation 3 y Xbox 360. Los dos\nepisodios se lanzaron de manera conjunta en la edición Metal Gear Solid V: The\nDefinitive Experience'),(2,'https://media.vandal.net/i/1280x720/3-2024/19/202431912572483_1.jpg.webp','PS5 Pro tendría una potente tecnologia de reescalado: PSSR podría ofrecer 4K a 120 fps y 8K a 60 fps','La tecnología de reescalado utilizando inteligencia artificial PlayStation Spectral\nSuper Resolution podría suponer un paso decisivo para PlayStation a partir de\nPS5 Pro, el modelo mejorado de la consola actual de Sony del que se han filtrado\nlas características y que podría alcanzar las 120 imágenes por segundo en los\njuegos a 4 K y 60 FPS a 8K, según informa Insidergaming.'),(5,'lalalalalala','lalalalalalala','hahahahahahahah');
+/*!40000 ALTER TABLE `noticia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `posts`
 --
 
 DROP TABLE IF EXISTS `posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `posts` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `text` text NOT NULL,
-  `type` varchar(255) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `text` text COLLATE utf8mb4_bin NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -128,6 +178,14 @@ LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'fidegames'
+--
+
+--
+-- Dumping routines for database 'fidegames'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -138,4 +196,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-05 20:22:34
+-- Dump completed on 2024-03-21 14:43:49
